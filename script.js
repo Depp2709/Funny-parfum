e301e356fa5f23a75cb18b81ab44ce44c4b554eb
Depp2709 <akmalmranggen@gmail.com>
2025-04-02 20:53:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function contactWhatsApp() {
-    const phoneNumber = "1234567890"; // Ganti dengan nomor WhatsApp Anda
+    const phoneNumber = "6287737141510"; // Ganti dengan nomor WhatsApp Anda
     const message = "Halo, saya tertarik untuk membeli parfum!";
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
